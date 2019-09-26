a895a663a0c2ec4b6169cf28ea434b0c8bbbeda4
Anonymous <user@example.com>
2019-09-27 01:40:06 +0900

    chore: Change tone names.

--- js/tonnetz.js
+++ js/tonnetz.js
@@ -3,7 +3,7 @@ var tonnetz = (function() {
 
   var module = {};
 
-  var TONE_NAMES = ['C', 'C♯', 'D', 'D♯', 'E', 'F', 'F♯', 'G', 'G♯', 'A', 'A♯', 'B'];
+  var TONE_NAMES = ['Do', 'Do♯', 'Re', 'Re♯', 'Mi', 'Fa', 'Fa♯', 'So', 'So♯', 'La', 'La♯', 'Si'];
   var STATE_OFF = 0,
       STATE_GHOST = 1,
       STATE_SUST = 2,
