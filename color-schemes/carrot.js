21915f0048c59095cb2cd4a3f6fd67be1dd84684
k-hatano <kent.ruffle.mgj626@gmail.com>
2020-04-16 21:58:57 +0900

    Fix & add some color schemes

---
+++ color-schemes/carrot.js
@@ -0,0 +1,40 @@
+colorscheme.addScheme("carrot", {
+  "name": "Carrot",
+  "background": "#00903e",
+  "nodes": {
+    "OFF": {
+      "label": "#007944",
+      "fill": "#78d300",
+      "stroke": "#008048"
+    },
+    "GHOST": {
+      "label": "#ffffff",
+      "fill": "#ffbd00",
+      "stroke": "#947000"
+    },
+    "SUSTAIN": {
+      "label": "#ffffff",
+      "fill": "#ffbf00",
+      "stroke": "#946f00"
+    },
+    "ON": {
+      "label": "#ffffff",
+      "fill": "#ff9300",
+      "stroke": "#945200"
+    }
+  },
+  "faces": {
+    "major": {
+      "label-off": "#d4fb79",
+      "label-on": "#945200",
+      "fill": "#ff9300",
+      "thin": "#007a45"
+    },
+    "minor": {
+      "label-off": "#73fa79",
+      "label-on": "#943f00",
+      "fill": "#ff6c00",
+      "thin": "#007900"
+    }
+  }
+});
