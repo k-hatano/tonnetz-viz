e4beb6b17643084eb183cab9891449cad3414b31
k-hatano <kent.ruffle.mgj626@gmail.com>
2021-02-17 03:21:05 +0900

    Add schemes

---
+++ color-schemes/matcha.js
@@ -0,0 +1,40 @@
+colorscheme.addScheme("matcha", {
+  "name": "Matcha",
+  "background": "#ffffff",
+  "nodes": {
+    "OFF": {
+      "label": "#c0c0c0",
+      "fill": "#ffffff",
+      "stroke": "#c0c0c0"
+    },
+    "GHOST": {
+      "label": "#ffffff",
+      "fill": "#b0c0a0",
+      "stroke": "#408020"
+    },
+    "SUSTAIN": {
+      "label": "#ffffff",
+      "fill": "#608050",
+      "stroke": "#408020"
+    },
+    "ON": {
+      "label": "#ffffff",
+      "fill": "#408010",
+      "stroke": "#408010"
+    }
+  },
+  "faces": {
+    "major": {
+      "label-off": "#c0c0c0",
+      "label-on": "#ffffff",
+      "fill": "#90ef60",
+      "thin": "#e7f0e3"
+    },
+    "minor": {
+      "label-off": "#c0c0c0",
+      "label-on": "#ffffff",
+      "fill": "#96e544",
+      "thin": "#f0f0f0"
+    }
+  }
+});
