3b3844a07ba9f7e5f8f7042eda4d02c99d179461
k-hatano <kent.ruffle.mgj626@gmail.com>
2021-03-11 02:35:25 +0900

    Add schemes, support "Accidental Notation" setting

---
+++ color-schemes/fluorescent.js
@@ -0,0 +1,40 @@
+colorscheme.addScheme("fluorescent", {
+  "name": "Fluorescent",
+  "background": "#000000",
+  "nodes": {
+    "OFF": {
+      "label": "#003b3b",
+      "fill": "#001717",
+      "stroke": "#002828"
+    },
+    "GHOST": {
+      "label": "#00a9a9",
+      "fill": "#004242",
+      "stroke": "#007979"
+    },
+    "SUSTAIN": {
+      "label": "#00a9a9",
+      "fill": "#004242",
+      "stroke": "#007979"
+    },
+    "ON": {
+      "label": "#00f1f1",
+      "fill": "#004242",
+      "stroke": "#00c3c3"
+    }
+  },
+  "faces": {
+    "major": {
+      "label-off": "#003333",
+      "label-on": "#00f1f1",
+      "fill": "#004e4e",
+      "thin": "#001919"
+    },
+    "minor": {
+      "label-off": "#003232",
+      "label-on": "#00f1f1",
+      "fill": "#003e3e",
+      "thin": "#001919"
+    }
+  }
+});
